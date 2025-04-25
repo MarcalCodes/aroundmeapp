4ebe64618e03620b081e89646051c4bd9b89f04f
Ingrid Marcal <marcal.delaine@gmail.com>
2025-04-25 17:05:25 +1000

    Configure DB migrations and add DB inial schemas

---
+++ Backend/db/migrations/20250425054138_init_db_schema.sql
@@ -0,0 +1,45 @@
+-- migrate:up
+CREATE TABLE "user"
+(
+    id         BIGSERIAL PRIMARY KEY,
+    email      TEXT                     NOT NULL UNIQUE,
+    name       TEXT                     NOT NULL,
+
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE "area"
+(
+    id         BIGSERIAL PRIMARY KEY,
+    postcode   TEXT UNIQUE,
+
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "subscription"
+(
+    userId     BIGINT                   NOT NULL references "user" (id),
+    areaId     BIGINT                   NOT NULL references "area" (id),
+
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (userId, areaId)
+);
+
+CREATE TABLE "event"
+(
+    id          BIGSERIAL PRIMARY KEY,
+    name        TEXT                     NOT NULL,
+    starts_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    ends_at     TIMESTAMP WITH TIME ZONE NOT NULL,
+    creator_id  BIGINT                   NOT NULL references "user" (id),
+    area_id     BIGINT                   NOT NULL references "area" (id),
+    canceled_at TIMESTAMP WITH TIME ZONE,
+
+    created_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP WITH TIME ZONE
+);
+
+
+-- migrate:down
