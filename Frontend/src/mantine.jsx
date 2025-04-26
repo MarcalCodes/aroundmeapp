5e5309803e3504934094def5123ac9b14ec5f9fd
Ingrid Marcal <marcal.delaine@gmail.com>
2025-04-26 17:52:17 +1000

    Fix application

--- Frontend/src/mantine.jsx
+++ Frontend/src/mantine.jsx
@@ -9,7 +9,4 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/dropzone/styles.css';
 import '@mantine/carousel/styles.css';
-import '@mantine/form/styles.css';
-import '@mantine/hooks/styles.css';
-import '@mantine/modals/styles.css';
 import '@mantine/notifications/styles.css';
