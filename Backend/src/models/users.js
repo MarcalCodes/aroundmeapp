a5c6af6870275e333144f8ab19a72eff60dcf0e1
Ingrid Marcal <marcal.delaine@gmail.com>
2025-04-29 16:01:18 +1000

    Update user DB schema and POST/ users endpoint. (#75)

--- Backend/src/models/users.js
+++ Backend/src/models/users.js
@@ -1,42 +1,44 @@
 import sql from "../db.js"
 
-const insert = async (email, name) =>
-    sql`INSERT INTO "user" (email, name)
-        VALUES (${email}, ${name});`
+const insert = async ({email, firstname, lastname, passwordHash}) =>
+  sql`INSERT INTO "user" (email, firstname, lastname, passwordhash)
+          VALUES (${email}, ${firstname}, ${lastname}, ${passwordHash})
+              RETURNING id, email, firstname, lastname;`;
 
-const update = async (id, email, name) =>
-    sql`UPDATE "user"
-        SET email     = ${email},
-            name      = ${name},
-            updated_at = ${new Date()}
-        WHERE id = ${id};`
+const update = async (id, email, firstname, lastname) =>
+  sql`UPDATE "user"
+      SET email      = ${email},
+          firstname  = ${firstname},
+          lastname   = ${lastname},
+          updated_at = ${new Date()}
+      WHERE id = ${id};`;
 
 const remove = async (id) =>
-    sql`DELETE
-        FROM "user"
-        WHERE id = ${id};`
+  sql`DELETE
+      FROM "user"
+      WHERE id = ${id};`;
 
 const get = async (id) =>
-    sql`SELECT *
-        FROM "user"
-        WHERE id = ${id};`
+  sql`SELECT id, email, firstname, lastname
+      FROM "user"
+      WHERE id = ${id};`;
 
 const all = async () =>
-    sql`SELECT *
-        FROM "user";`
+  sql`SELECT id, email, firstname, lastname
+      FROM "user";`;
 
 const getUsersByAreaSubscription = async (areaId) =>
-    sql`SELECT u.*
-        FROM subscription as s,
-             "user" as u
-        WHERE s.area_id = ${areaId}
-          AND s.user_id = u.id;` // joining subscription and user tables on subscription::userId and user::id fields
+  sql`SELECT u.id, u.email, u.firstname, u.lastname
+      FROM subscription As s
+               JOIN "user" As u ON s.user_id = u.id
+      WHERE s.area_id = ${areaId};`;
+
 
 export {
-    all,
-    get,
-    insert,
-    update,
-    remove,
-    getUsersByAreaSubscription
+  all,
+  get,
+  insert,
+  update,
+  remove,
+  getUsersByAreaSubscription
 };
